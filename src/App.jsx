9ae0318bc79e9ea8cf35bd98b3a0b8f3eef7361a
Bethel Gelan <bethelgelan16@gmail.com>
2024-11-22 11:50:57 +0300

    Carousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Header from "./Components/Header/Header";
 
-import CarouselEffect from "./Components/Carousel/CarouselEffect";
+
+import CarouselBanner from "./Components/Carousel/Carousel";
 
 function App() {
   return (
     <>
       <Header />
-      <CarouselEffect />
+      <CarouselBanner />
     </>
   );
 }
